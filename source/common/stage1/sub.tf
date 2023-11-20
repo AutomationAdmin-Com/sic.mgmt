dd0e2af8923925bcd8c7f352bb690ec2732d068f
Gerry Williams <gerryw1389@gmail.com>
2023-11-20 06:06:49 -0600

    try add subs4

--- source/common/stage1/sub.tf
+++ source/common/stage1/sub.tf
@@ -1,21 +1,21 @@
 
 # data "azurerm_billing_mca_account_scope" "billing" {
-#   billing_account_name = "aa-billing-account-1"
-#   billing_profile_name = "aa-billing-profile-1"
-#   invoice_section_name = "aa-invoice-section-1"
+#   billing_account_name = var.billing_scope_id
+#   billing_profile_name = "OEHH-C5SJ-BG7-PGB"
+#   invoice_section_name = "JFC2-PT5G-PJA-PGB"
 # }
 
 resource "azurerm_subscription" "nonprd_hub" {
   subscription_name = "nonprd-hub"
-  billing_scope_id  = var.billing_scope_id # data.azurerm_billing_mca_account_scope.billing.id
+  billing_scope_id  = data.azurerm_billing_mca_account_scope.billing.id
 }
 
 resource "azurerm_subscription" "nonprd_spk" {
   subscription_name = "nonprd-spoke"
-  billing_scope_id  = var.billing_scope_id
+  billing_scope_id  = data.azurerm_billing_mca_account_scope.billing.id
 }
 
 resource "azurerm_subscription" "prd_spk" {
   subscription_name = "prd-spoke"
-  billing_scope_id  = var.billing_scope_id
+  billing_scope_id  = data.azurerm_billing_mca_account_scope.billing.id
 }
